1f8b99a16aaf1d80035151481cc29b0f0a36f50c
dymafr <erwan1309@hotmail.com>
2022-03-02 15:39:54 +0100

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <button @click="toggle = !toggle">Toggle</button>
+  <div v-if="toggle">
+    <Blog />
+  </div>
+</template>
+
+<script setup lang="ts">
+import { ref } from 'vue';
+import Blog from './Blog.vue';
+
+const toggle = ref(true);
+</script>
+
+<style scoped lang="scss"></style>
---
+++ src/Blog.vue
@@ -0,0 +1,45 @@
+<template>
+  <h1>Blog</h1>
+  <button @click="count++">+1</button>
+  <p>{{ count }}</p>
+</template>
+
+<script setup lang="ts">
+import {
+  onBeforeMount,
+  onBeforeUnmount,
+  onBeforeUpdate,
+  onMounted,
+  onUnmounted,
+  onUpdated,
+  ref,
+} from 'vue';
+
+const count = ref(0);
+
+const intervalId = setInterval(() => {
+  console.log('tick');
+}, 1000);
+
+onBeforeMount(() => {
+  console.log('Avant le montage');
+});
+onMounted(() => {
+  console.log('Après le montage');
+});
+onBeforeUpdate(() => {
+  console.log('Avant la mise à jour');
+});
+onUpdated(() => {
+  console.log('Après la mise à jour');
+});
+onBeforeUnmount(() => {
+  console.log('Avant la destruction / démontage');
+});
+onUnmounted(() => {
+  clearInterval(intervalId);
+  console.log('Démontage');
+});
+</script>
+
+<style scoped lang="scss"></style>
